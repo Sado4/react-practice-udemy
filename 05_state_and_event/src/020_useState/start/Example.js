2ce6da3526570dbba0ceb240f48725a09076500b
Sado4 <derasado04@gmail.com>
2022-10-02 10:27:24 +0900

    feat: useState

--- 05_state_and_event/src/020_useState/start/Example.js
+++ 05_state_and_event/src/020_useState/start/Example.js
@@ -1,8 +1,16 @@
-const Example = () => {
+import { useState } from 'react';
 
+const Example = () => {
+  // let [val, setFn] = useState('');
+  const [val, setval] = useState('')
   return (
     <>
-      
+      <input
+        type="text"
+        onChange={(e) => {
+          setval(e.target.value);
+        }}
+      /> = {val}
     </>
   );
 };
