d2acb12f2d7d9f854e9a4505ff372da3d5bff7a0
Sado4 <derasado04@gmail.com>
2022-10-08 00:56:11 +0900

    feat: state array

--- 05_state_and_event/src/064_state_array/start/Example.js
+++ 05_state_and_event/src/064_state_array/start/Example.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
+
 const Example = () => {
   const numArray = [1, 2, 3, 4, 5];
+  const [nums, setNums] = useState(numArray);
+
+  const shuffle = () => {
+    const newNums = [...nums];
+    const popNumber = newNums.pop();
+    newNums.unshift(popNumber);
+    setNums(newNums);
+  }
 
   return (
     <>
-      
+      <h1>{nums}</h1>
+      <button onClick={shuffle}>shuffle</button>
     </>
   );
 };
