b49e451a24c7064dc8f0c5fa8390f68b5921011f
Sado4 <derasado04@gmail.com>
2022-10-10 23:54:48 +0900

    feat: state components

--- 05_state_and_event/src/070_state_and_component/start/Example.js
+++ 05_state_and_event/src/070_state_and_component/start/Example.js
@@ -1,6 +1,24 @@
 import { useState } from "react";
 
 const Example = () => {
+  const [toggle, setToggle] = useState(true);
+  const switchToggle = () => {
+    setToggle((bool) => {
+      return !bool;
+    });
+    // setToggle((bool) => {
+    //   console.log(bool);
+    // });
+  }
+  return (
+    <>
+      <button onClick={switchToggle}>Switch</button>
+      {toggle ? <Count key="A" title="TRUE" /> : <Count key="B" title="FALSE" />}
+    </>
+  )
+}
+
+const Count = ({ title }) => {
   const [count, setCount] = useState(0);
   const countUp = () => {
     setCount((prevstate) => prevstate + 1);
@@ -10,7 +28,7 @@ const Example = () => {
   };
   return (
     <>
-      <h3>カウント: {count}</h3>
+      <h3>{title}: {count}</h3>
       <button onClick={countUp}>+</button>
       <button onClick={countDown}>-</button>
     </>
