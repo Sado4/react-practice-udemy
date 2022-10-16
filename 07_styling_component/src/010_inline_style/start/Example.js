27688792fb07766291617c7bbdaa377c2a91ce50
Sado4 <derasado04@gmail.com>
2022-10-16 17:43:45 +0900

    feat: JSX CSS inline

--- 07_styling_component/src/010_inline_style/start/Example.js
+++ 07_styling_component/src/010_inline_style/start/Example.js
@@ -1,16 +1,29 @@
-import { useState } from "react"
+import { useState } from 'react';
 
 const Example = () => {
-    const [isSelected, setIsSelected] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
 
-    const clickHandler = () => setIsSelected(prev => !prev);
+  const clickHandler = () => setIsSelected((prev) => !prev);
 
-    return (
-        <>
-            <button onClick={clickHandler}>ボタン</button>
-            <div>{isSelected && "クリックされました。"}</div>
-        </>
-    )
+  const style = {
+    margin: 'auto',
+    width: 120,
+    height: 60,
+    borderRadius: 999,
+    display: 'block',
+    border: 'none',
+    backgroundColor: isSelected ? 'pink' : '',
+    cursor: 'pointer',
+  };
+
+  return (
+    <>
+      <button style={style} onClick={clickHandler}>
+        ボタン
+      </button>
+      <div style={{ color: 'red' }}>{isSelected && 'クリックされました。'}</div>
+    </>
+  );
 };
 
 export default Example;
