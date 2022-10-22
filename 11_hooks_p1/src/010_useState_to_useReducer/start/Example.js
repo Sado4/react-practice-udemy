39d38061c72dc9c81d048d1918893746a128a8e5
Sado4 <derasado04@gmail.com>
2022-10-22 17:07:40 +0900

    feat: functional programing & React Hooks

--- 11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
+++ 11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
@@ -1,7 +1,32 @@
+import { useState, useReducer } from 'react';
+
 const Example = () => {
-  
+  const [first, setfirst] = useState(0);
+  const [second, dispatch] = useReducer((prev, action) => {
+    if (action === 'countUp') {
+      return ++prev;
+    }
+    if (action === 'countDown') {
+      return --prev;
+    }
+  }, 0);
+
+  const countUpFirst = () => {
+    setfirst(first + 1);
+  };
+  const countUpSecond = () => {
+    dispatch('countUp');
+  };
+  const countDown = () => {
+    dispatch('countDown');
+  };
   return (
     <>
+      <div>{first}</div>
+      <button onClick={countUpFirst}>+</button>
+      <div>{second}</div>
+      <button onClick={countUpSecond}>+</button>
+      <button onClick={countDown}>-</button>
     </>
   );
 };
