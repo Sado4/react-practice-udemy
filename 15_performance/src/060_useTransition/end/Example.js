8d1f53171e5ad93ce031680a39bde50341baf4aa
Sado4 <derasado04@gmail.com>
2022-10-30 22:57:05 +0900

    feat: useTransition

--- 15_performance/src/060_useTransition/end/Example.js
+++ 15_performance/src/060_useTransition/end/Example.js
@@ -1,5 +1,5 @@
 // POINT [Concurrent Mode]useTransitionでユーザビリティを向上
-import { useState, useTransition } from "react";
+import { useState, useTransition } from 'react';
 
 const generateDummyItem = (num) => {
   return new Array(num).fill(null).map((item, index) => `item ${index}`);
@@ -8,23 +8,26 @@ const generateDummyItem = (num) => {
 const dummyItems = generateDummyItem(10000);
 
 const Example = () => {
+  // 第２引数の関数でコールバック関数中に実行したい処理を記述すると、その優先度を下げることができる
+  // きほんは使わないように、useMemoなどを使う
   const [isPending, startTransition] = useTransition();
-  const [filterVal, setFilterVal] = useState("");
+  const [filterVal, setFilterVal] = useState('');
 
   const changeHandler = (e) => {
     startTransition(() => {
       setFilterVal(e.target.value);
-    })
+    });
   };
 
   return (
     <>
       <input type="text" onChange={changeHandler} />
+      {/* isPendingは関数が処理中の場合は、true 処理が終わったらfalseが返される */}
       {isPending && <div>Loading...</div>}
       <ul>
         {dummyItems
           .filter((item) => {
-            if (filterVal === "") return true;
+            if (filterVal === '') return true;
             return item.includes(filterVal);
           })
           .map((item) => (
