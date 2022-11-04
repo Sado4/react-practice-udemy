59f92f6f95c9c9227b7bc8a5a37620e2c1c4ab62
Sado4 <derasado04@gmail.com>
2022-11-05 00:24:45 +0900

    feat: Link & useRouter

--- 17_nextjs_p1/start/next.config.js
+++ 17_nextjs_p1/start/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // ダミーURLを直うちされた際に、実際のページURLを表示するために必要
+  async rewrites() {
+    return [
+      {
+        source: '/personal',
+        destination: '/08_multipage?step=1',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
