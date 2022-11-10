5f5bdb16ba505fd31b8cd022c69b0d20acffd83d
Sado4 <derasado04@gmail.com>
2022-11-10 09:51:46 +0900

    feat: Next.js SSR && getServerSideProps

--- 18_nextjs_p2/end/src/pages/010_SSR/index.js
+++ 18_nextjs_p2/end/src/pages/010_SSR/index.js
@@ -1,37 +1,43 @@
 // POINT [SSR]とgetServerSideProps
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react';
 
 // export default function SSR({message}) {
-export default function SSR() {
-    console.log('hello')
-    // console.log(message)
+// 初初期表示のときは、SSRが実行される
+// 画面遷移時はCSRで表示される
+export default function SSR({ message }) {
+  console.log('hello');
+  console.log(message);
 
-    useEffect(() => {
-        console.log('useEffect');
-        window.localStorage.setItem('key', 'value');
-        document.cookie = 'val=0; path=/;'
-    }, [])
+  // windowはブラウザ上(クライアント側)でしか使えない(SSR(サーバー側(Node.js))では使えない)
+  //   window.document.title = 'SSR';
+  // console.log(message)
 
-    const [state, setState] = useState('bye');
-    const val = 0
-    return (
-        <>
-            <h3>SSR</h3>
-            <h3>{state}</h3>
-        </>
-    )
+  // useEffectはCSR(クライアント側)でしか実行されない
+  useEffect(() => {
+    console.log('useEffect');
+    window.localStorage.setItem('key', 'value');
+    document.cookie = 'val=0; path=/;';
+  }, []);
+
+  const [state, setState] = useState('bye');
+  const val = 0;
+  return (
+    <>
+      <h3>SSR</h3>
+      <h3>{state}</h3>
+    </>
+  );
 }
 
-// export async function getServerSideProps(context) {
-
-//     const { cookie } = context.req.headers;
-//     console.log('cookie', cookie);
-//     console.log('getServerSideProps is executed')
-//     return {
-//         // redirect: {
-//         //     destination: '/',
-//         //     permanent: false
-//         // },
-//         props: { message: 'From Server Side Props' }
-//     }
-// }
+export async function getServerSideProps(context) {
+  const { cookie } = context.req.headers;
+  console.log('cookie', cookie);
+  console.log('getServerSideProps is executed');
+  return {
+    redirect: {
+      destination: '/',
+      permanent: false,
+    },
+    props: { message: 'From Server Side Props' },
+  };
+}
