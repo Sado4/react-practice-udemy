56f1c93ad9b4eed3cb1a94a75da2dcb5d98d0fe8
Sado4 <derasado04@gmail.com>
2022-11-12 23:26:12 +0900

    feat: getStaticPaths

--- 18_nextjs_p2/end/src/pages/020_SG/[id].js
+++ 18_nextjs_p2/end/src/pages/020_SG/[id].js
@@ -1,29 +1,37 @@
 // POINT [SG]ダイナミックルーティングの記述方法
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router';
 
 export default function Page({ id, date }) {
   const router = useRouter();
   if (router.isFallback) {
     return <h3>Loading...</h3>;
   }
-  return <h3>このページは{id}です。{date}</h3>;
+  return (
+    <h3>
+      このページは{id}です。{date}
+    </h3>
+  );
 }
 
+// ダイナミックルーティングのパスを指定
+// SGでダイナミックルーティングを実装する場合は、
+// getStaticPathsでパスを指定する必要がある(ビルド時にHTMLが生成され、決められたパスしかアクセスできないため)
 export async function getStaticPaths() {
-  console.log("getStaticPaths executed");
+  console.log('getStaticPaths executed');
   return {
-    paths: [{ params: { id: "1" } }, { params: { id: "2" } }],
-    fallback: "blocking",
+    paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
+    fallback: 'blocking',
   };
 }
 
+// paramsでパスの値を受け取り、propsでparamsのidを返す
 export async function getStaticProps({ params }) {
-  console.log("getStaticProps executed");
-  const date = new Date;
+  console.log('getStaticProps executed');
+  const date = new Date();
   return {
     props: {
       id: params.id,
-      date: date.toJSON()
+      date: date.toJSON(),
     },
     // revalidate: 5
   };
